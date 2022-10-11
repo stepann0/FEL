322e69c7b28d27944812caa04846d290f679da2b
Stepa <stepa8000@gmail.com>
2022-10-11 20:27:50 +0400

    Add grammar for arrays

--- parser.py
+++ parser.py
@@ -2,15 +2,15 @@ import ply.yacc as yacc
 from lexer import tokens
 import exec
 
-precedence = (
-    ("left", "OR"),
-    ("left", "AND"),
-    ("left", "EQEQ", "NOT_EQ"),
-    ("left", "GT", "GE", "LT", "LE"),
-    ("left", "PLUS", "MINUS"),
-    ("left", "MULT", "DIVIDE", "MOD"),
-    ("right", "POW")
-)
+# precedence = (
+#     ("left", "OR"),
+#     ("left", "AND"),
+#     ("left", "EQEQ", "NOT_EQ"),
+#     ("left", "GT", "GE", "LT", "LE"),
+#     ("left", "PLUS", "MINUS"),
+#     ("left", "MULT", "DIVIDE", "MOD"),
+#     ("right", "POW")
+# )
 
 def p_expression(p):
     """expression : disjunction"""
@@ -42,13 +42,13 @@ def p_invertion(p):
 
 def p_comp_operator(p):
     """COMP_OP : EQEQ
-            | NOT_EQ
-            | LE
-            | LT
-            | GE
-            | GT
-            | IN
-            | IS"""
+               | NOT_EQ
+               | LE
+               | LT
+               | GE
+               | GT
+               | IN
+               | IS"""
     p[0] = p[1]
 
 def p_comparison(p):
@@ -75,7 +75,7 @@ def p_bit_xor(p):
     else:
         p[0] = p[1]
 
-def p_statment_list(p):
+def p_bit_and(p):
     """bitwise_and : bitwise_and BIT_AND sum 
                    | sum"""
     if len(p) == 4:
@@ -120,16 +120,37 @@ def p_pow(p):
     else:
         p[0] = p[1]
 
-def p_atom(p):
-    """atom : NUMBER
-            | TRUE 
-            | FALSE
-            | LPAREN expression RPAREN"""
+def p_atom_num(p):
+    """atom : NUMBER"""
+    p[0] = p[1]
 
-    if len(p) == 4:
-        p[0] = p[2]
-    else:
-        p[0] = p[1]
+def p_atom_bool(p):
+    """atom : TRUE 
+            | FALSE"""
+    p[0] = p[1] == 'true'
+
+def p_atom_parenth_expr(p):
+    """atom : LPAREN expression RPAREN"""
+    p[0] = p[2]
+
+def p_atom_array(p):
+    """atom : array"""
+    p[0] = p[1]
+
+def p_array(p):
+    """array : LSQUARE expression_list RSQUARE"""
+    p[0] = p[2]
+
+def p_expression_list(p):
+    """expression_list : expression_list COMMA expression
+                       | expression
+                       |"""
+    if len(p) == 1: # no args
+        p[0] = []
+    elif len(p) == 2: # one arg
+        p[0] = [p[1]]
+    else: # 2+ args, separated by ","
+        p[0] = [*p[1], p[3]]
 
 def p_number(p):
     """NUMBER : INT
